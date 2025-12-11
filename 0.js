ee24e05a792797aa1eb36b4cc072b3811f6b2988
donalyx <danixsr13@gmail.com>
2025-12-11 08:18:42 -0600

    Add files via upload

--- 0.js
+++ 0.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([23.6345, -102.5528], 5); 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '© OpenStreetMap'
+attribution: '� OpenStreetMap'
 }).addTo(map);
 
 if (navigator.geolocation) {
@@ -13,20 +13,20 @@ var lon = pos.coords.longitude;
 map.setView([lat, lon], 14);
 
 L.marker([lat, lon]).addTo(map)
-.bindPopup("Tu ubicación actual")
+.bindPopup("Tu ubicaci�n actual")
 .openPopup();
 },
 function () {
-alert("No se pudo obtener tu ubicación. Mapa centrado en México.");
+alert("No se pudo obtener tu ubicaci�n. Mapa centrado en M�xico.");
 }
 );
 } else {
-alert("Tu navegador no soporta geolocalización.");
+alert("Tu navegador no soporta geolocalizaci�n.");
 }
 
 var recicladoras = [
     {
-        nombre: "Ecoplast – Cuautitlán Izcalli",
+        nombre: "Ecoplast  Cuautitl�n Izcalli",
         coords: [19.65585, -99.21990] 
     },
     {
@@ -38,13 +38,33 @@ var recicladoras = [
         coords: [19.64695, -99.21485]
     },
     {
-        nombre: "Banco de Materiales Reciclables – Tultitlán",
+        nombre: "Banco de Materiales Reciclables  Tultitl�n",
         coords: [19.64260, -99.16210]
     },
     {
-        nombre: "Centro de Acopio PET – PetStar Ecatepec",
+        nombre: "Centro de Acopio PET  PetStar Ecatepec",
         coords: [19.54220, -99.06640]
-    }
+    },
+    {
+    nombre: "RESICLAJE Compra y Venta de PET",
+    coords: [19.5628, -99.1895] 
+    },
+    {
+    nombre: "Waste & Recycling",
+    coords: [19.6552, -99.2148] 
+},
+{
+    nombre: "Reemplazo Centro de Reciclaje",
+    coords: [19.6560, -99.2159] 
+},
+{
+    nombre: "Depósito de Reciclaje “Izcalli”",
+    coords: [19.6586, -99.2392] 
+},
+{
+    nombre: "Alcamare – Banco de Materiales Reciclables (Tultitlán)",
+    coords: [19.6460, -99.1676] 
+},
 ];
 
 recicladoras.forEach(function (lugar) {
